371236d18769dc0796bae712269817b1b959ec39
Andrés <a.lopez.martinich@gmail.com>
2025-11-11 17:35:08 -0300

    Se muestra el desafio a seguir

---
+++ vocales-y-consonantes.py
@@ -0,0 +1,8 @@
+"""
+Desafío: Dado un string, que devuelva la cantidad de vocales y consonantes en un string.
+Ejemplos:
+Andres: 3 vocales y 3 consonantes
+hermana: 3 vocales y 4 consonantes
+murcielago: 5 vocales y 5 consonantes
+Las motos son dificiles de manejar: 12 vocales y 17 consonantes
+"""
